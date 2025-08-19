66ea73d3a2f41eb9b441083ec30edfb5d6d64969
Gustavo Bruno <gugubrunopf@gmail.com>
2025-08-19 16:58:47 -0300

    feat: innitiating frontend

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google';
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: "IluminaCasa",
+  description: "A melhor loja digital para venda de produtos para a sua casa!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${poppins.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
